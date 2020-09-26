f438e88f6afecd8ca228e6aae186c8dd79f1b910
Vu T Doan <vdoan@masonlive.gmu.edu>
2020-09-26 14:51:03 -0400

    Edit Assign Role designs

--- Models/RoleViewModel.cs
+++ Models/RoleViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -14,6 +15,7 @@ namespace TeamComeback.Models
             Name = role.Name;
         }
         public string Id { get; set; }
+        [Display(Name = "Role")]
         public string Name { get; set; }
     }
 }
