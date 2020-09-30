9dd3ac85a637f6c2abcef195f517db382173d81e
Vu T Doan <vdoan@masonlive.gmu.edu>
2020-09-29 20:48:55 -0400

    Fix spelling

--- Common.cs
+++ Common.cs
@@ -11,7 +11,7 @@ namespace TeamComeback
         public static List<Answer> GetAnswers()
         {
             List<Answer> list = new List<Answer>();
-            list.Add(new Answer() { ID = 1, Name = "Execellent", Css = "check w3"});
+            list.Add(new Answer() { ID = 1, Name = "Excellent", Css = "check w3"});
             list.Add(new Answer() { ID = 2, Name = "Good", Css = "check w3ls" });
             list.Add(new Answer() { ID = 3, Name = "Neutral", Css = "check wthree" });
             list.Add(new Answer() { ID = 4, Name = "Poor", Css = "check w3_agileits" });
